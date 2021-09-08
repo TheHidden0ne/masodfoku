59ef52aa66422f03293db0d0389150f0cb21abb4
Zsófia Limbek <zsofia.limbek@gmail.com>
2021-09-08 16:02:32 +0200

    added error message if 'a' was zero

--- masodfoku.py
+++ masodfoku.py
@@ -13,18 +13,25 @@ def bekeres(valtozo_neve):
     return user_input
 
 
-a = bekeres("a")
+a = None
+while a is None:
+    temp = bekeres("a")
+    if temp != 0:
+        a = temp
+    else:
+        print("'a' nem lehet nulla")
+
 b = bekeres("b")
 c = bekeres("c")
-D = math.pow(b, 2) - 4*a*c
+D = math.pow(b, 2) - 4 * a * c
 
 if D < 0:
     print(f"Nincs megoldás, D={D}")
 elif D == 0:
-    x = (-b + math.sqrt(D)) / (2*a)
+    x = (-b + math.sqrt(D)) / (2 * a)
     print(f"Egy megoldás van: {x}")
-else :
-    x1 = (-b + math.sqrt(D)) / (2*a)
-    x2 = (-b - math.sqrt(D)) / (2*a)
+else:
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
 
     print(f"Megoldások: {x1}, {x2}")
