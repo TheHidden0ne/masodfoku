050bb2730ae4523c2f885062617df01ade19bec7
Zsófia Limbek <zsofia.limbek@gmail.com>
2021-09-08 15:50:23 +0200

    added loop to input check

--- masodfoku.py
+++ masodfoku.py
@@ -3,17 +3,20 @@ import math
 
 def bekeres(valtozo_neve):
     user_input = input(f"{valtozo_neve} változó értéke: ")
-    try:
-        user_input = int(user_input)
-    except ValueError:
-        print("Számot adjon meg")
+
+    while not type(user_input) == float:
+        try:
+            user_input = float(user_input)
+        except ValueError:
+            user_input = input(f"{valtozo_neve} változó értéke, de légyszi szám legyen: ")
+
     return user_input
 
 
 a = bekeres("a")
 b = bekeres("b")
 c = bekeres("c")
-D = b^2 - 4*a*c
+D = math.pow(b, 2) - 4*a*c
 
 if D < 0:
     print(f"Nincs megoldás, D={D}")
